efe6e882fe584e7d175a60311288ca58f0312402
estar73 <estarkov@gmail.com>
2019-09-30 11:51:44 +0000

    = regular update

--- PDF Optimization API/cURL/Optimize PDF from File/Optimize-PDF-from-File.cmd
+++ PDF Optimization API/cURL/Optimize PDF from File/Optimize-PDF-from-File.cmd
@@ -5,10 +5,10 @@ set CURL="curl.exe"
 
 :: The authentication key (API Key).
 :: Get your own by registering at https://app.pdf.co/documentation/api
-set API_KEY=***********************************
+set API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-:: Direct URL of source PDF file.
-set SOURCE_FILE_URL=https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-optimize/sample.pdf
+:: PDF file.
+set SOURCE_FILE=sample.pdf
 :: PDF document password. Leave empty for unprotected documents.
 set PASSWORD=
 :: Result PDF file name
@@ -16,10 +16,11 @@ set RESULT_FILE_NAME=result.pdf
 
 
 :: Prepare URL for `Optimize PDF` API call
-set QUERY="https://api.pdf.co/v1/pdf/optimize?name=%RESULT_FILE_NAME%&password=%PASSWORD%&url=%SOURCE_FILE_URL%"
+set QUERY="https://api.pdf.co/v1/pdf/optimize?name=%RESULT_FILE_NAME%&password=%PASSWORD%&pages=%PAGES%"
+echo %QUERY%
 
 :: Perform request and save response to a file
-%CURL% -# -X GET -H "x-api-key: %API_KEY%" %QUERY% >response.json
+%CURL% -# -X GET -H "x-api-key: %API_KEY%" -F "file=@%SOURCE_FILE%" %QUERY% >response.json
 
 :: Display the response
 type response.json
--- PDF Splitting API/cURL/Split PDF from File/Split-PDF-from-File.cmd
+++ PDF Splitting API/cURL/Split PDF from File/Split-PDF-from-File.cmd
@@ -5,21 +5,24 @@ set CURL="curl.exe"
 
 :: The authentication key (API Key).
 :: Get your own by registering at https://app.pdf.co/documentation/api
-set API_KEY=***********************************
+set API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-:: Direct URL of source PDF file.
-set SOURCE_FILE_URL=https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-optimize/sample.pdf
+:: PDF file.
+set SOURCE_FILE=sample.pdf
+:: Comma-separated list of page numbers (or ranges) to process. Example: '1,3-5,7-'.
+set PAGES=1-2,3-
 :: PDF document password. Leave empty for unprotected documents.
 set PASSWORD=
 :: Result PDF file name
 set RESULT_FILE_NAME=result.pdf
 
 
-:: Prepare URL for `Optimize PDF` API call
-set QUERY="https://api.pdf.co/v1/pdf/optimize?name=%RESULT_FILE_NAME%&password=%PASSWORD%&url=%SOURCE_FILE_URL%"
+:: Prepare URL for `Split PDF` API call
+set QUERY="https://api.pdf.co/v1/pdf/split?name=%RESULT_FILE_NAME%&password=%PASSWORD%&pages=%PAGES%"
+echo %QUERY%
 
 :: Perform request and save response to a file
-%CURL% -# -X GET -H "x-api-key: %API_KEY%" %QUERY% >response.json
+%CURL% -# -X GET -H "x-api-key: %API_KEY%" -F "file=@%SOURCE_FILE%" %QUERY% >response.json
 
 :: Display the response
 type response.json
