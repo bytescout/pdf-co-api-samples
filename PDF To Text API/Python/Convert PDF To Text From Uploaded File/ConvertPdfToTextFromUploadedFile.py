3e6670fa8d2a7e379742364f98ff1a137da3769d
estar73 <estarkov@gmail.com>
2019-11-09 04:14:31 +0000

    = regular update

--- PDF To Text API/Python/Convert PDF To Text From Uploaded File/ConvertPdfToTextFromUploadedFile.py
+++ PDF To Text API/Python/Convert PDF To Text From Uploaded File/ConvertPdfToTextFromUploadedFile.py
@@ -1,10 +1,5 @@
-""" Cloud API asynchronous "PDF To Text" job example.
-    Allows to avoid timeout errors when processing huge or scanned PDF documents.
-"""
 import os
 import requests # pip install requests
-import time
-import datetime
 
 # The authentication key (API Key).
 # Get your own by registering at https://app.pdf.co/documentation/api
@@ -21,8 +16,6 @@ Pages = ""
 Password = ""
 # Destination TXT file name
 DestinationFile = ".\\result.txt"
-# (!) Make asynchronous job
-Async = True
 
 
 def main(args = None):
@@ -35,9 +28,8 @@ def convertPdfToText(uploadedFileUrl, destinationFile):
     """Converts PDF To Text using PDF.co Web API"""
 
     # Prepare URL for 'PDF To Text' API request
-    url = "{}/pdf/convert/to/text?async={}&name={}&password={}&pages={}&url={}".format(
+    url = "{}/pdf/convert/to/text?name={}&password={}&pages={}&url={}".format(
         BASE_URL,
-        Async,
         os.path.basename(destinationFile),
         Password,
         Pages,
@@ -50,57 +42,22 @@ def convertPdfToText(uploadedFileUrl, destinationFile):
         json = response.json()
 
         if json["error"] == False:
-            # Asynchronous job ID
-            jobId = json["jobId"]
-            #  URL of the result file
-            resultFileUrl = json["url"]
-            
-            # Check the job status in a loop. 
-            # If you don't want to pause the main thread you can rework the code 
-            # to use a separate thread for the status checking and completion.
-            while True:
-                status = checkJobStatus(jobId) # Possible statuses: "working", "failed", "aborted", "success".
-                
-                # Display timestamp and status (for demo purposes)
-                print(datetime.datetime.now().strftime("%H:%M.%S") + ": " + status)
-                
-                if status == "success":
-                    # Download result file
-                    r = requests.get(resultFileUrl, stream=True)
-                    if (r.status_code == 200):
-                        with open(destinationFile, 'wb') as file:
-                            for chunk in r:
-                                file.write(chunk)
-                        print(f"Result file saved as \"{destinationFile}\" file.")
-                    else:
-                        print(f"Request error: {response.status_code} {response.reason}")
-                    break
-                elif status == "working":
-                    # Pause for a few seconds
-                    time.sleep(3)
-                else:
-                    print(status)
-                    break
+            #  Get URL of result file
+            resultFileUrl = json["url"]            
+            # Download result file
+            r = requests.get(resultFileUrl, stream=True)
+            if (r.status_code == 200):
+                with open(destinationFile, 'wb') as file:
+                    for chunk in r:
+                        file.write(chunk)
+                print(f"Result file saved as \"{destinationFile}\" file.")
+            else:
+                print(f"Request error: {response.status_code} {response.reason}")
         else:
             # Show service reported error
             print(json["message"])
     else:
         print(f"Request error: {response.status_code} {response.reason}")
-
-
-def checkJobStatus(jobId):
-    """Checks server job status"""
-
-    url = f"{BASE_URL}/job/check?jobid={jobId}"
-    
-    response = requests.get(url, headers={ "x-api-key": API_KEY })
-    if (response.status_code == 200):
-        json = response.json()
-        return json["status"]
-    else:
-        print(f"Request error: {response.status_code} {response.reason}")
-
-    return None
 
 
 def uploadFile(fileName):
