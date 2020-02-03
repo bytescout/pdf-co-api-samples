c2c82b3fdb283f82017a237fca59ceb3535a220d
estar73 <estarkov@gmail.com>
2020-02-03 04:11:05 +0000

    = regular update

---
+++ Barcode Reader API/C#/Async File Uploading/Program.cs
@@ -0,0 +1,87 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace ByteScoutWebApiExample
+{
+	class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co/documentation/api
+		const String API_KEY = "***********************************";
+		
+		// Source file name
+		const string SourceFile = @".\sample.pdf";
+
+		static void Main(string[] args)
+		{
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			// 1. RETRIEVE THE PRESIGNED URL TO UPLOAD THE FILE.
+			// * If you already have a direct file URL, skip to the step 3.
+			
+			// Prepare URL for `Get Presigned URL` API call
+			string query = Uri.EscapeUriString(string.Format(
+				"https://api.pdf.co/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name={0}", 
+				Path.GetFileName(SourceFile)));
+
+			try
+			{
+				// Execute request
+				string response = webClient.DownloadString(query);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Get URL to use for the file upload
+					string uploadUrl = json["presignedUrl"].ToString();
+					// Get URL of uploaded file to use with later API calls
+					string uploadedFileUrl = json["url"].ToString();
+
+					// 2. UPLOAD THE FILE TO CLOUD.
+					webClient.Headers.Add("content-type", "application/octet-stream");
+					webClient.UploadFileAsync(new Uri(uploadUrl), "PUT", SourceFile); // You can use UploadData() instead if your file is byte[] or Stream
+
+                    // Display uploaded file url, which will be used to read barcodes from
+                    Console.WriteLine("Uploaded File URL: {0}", uploadedFileUrl);
+                }
+				else
+				{
+					// Display service reported error
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				// Display request error
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+            Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Barcode Reader API/VB.NET/Async File Uploading/Module1.vb
@@ -0,0 +1,76 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+
+    ' The authentication key (API Key).
+    ' Get your own by registering at https://app.pdf.co/documentation/api
+    Const API_KEY As String = "************************************"
+
+    ' Source file name
+    Const SourceFile as string = ".\sample.pdf"
+
+    Sub Main()
+
+        ' Create standard .NET web client instance
+        Dim webClient As WebClient = New WebClient()
+
+        ' Set API Key
+        webClient.Headers.Add("x-api-key", API_KEY)
+
+        ' 1. RETRIEVE THE PRESIGNED URL TO UPLOAD THE FILE.
+        ' * If you already have a direct file URL, skip to the step 3.
+
+        ' Prepare URL for `Get Presigned URL` API call
+        Dim query As String = Uri.EscapeUriString(String.Format(
+            "https://api.pdf.co/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name={0}",
+            Path.GetFileName(SourceFile)))
+
+        Try
+            ' Execute request
+            Dim response As String = webClient.DownloadString(query)
+
+            ' Parse JSON response
+            Dim json As JObject = JObject.Parse(response)
+
+            If json("error").ToObject(Of Boolean) = False Then
+                ' Get URL to use for the file upload
+                Dim uploadUrl As String = json("presignedUrl").ToString()
+                ' Get URL of uploaded file to use with later API calls
+                Dim uploadedFileUrl As String = json("url").ToString()
+
+                ' 2. UPLOAD THE FILE TO CLOUD.
+                webClient.Headers.Add("content-type", "application/octet-stream")
+                webClient.UploadFileAsync(New Uri(uploadUrl), "PUT", SourceFile) ' You can use UploadData() instead if your file is byte array or Stream
+
+                ' Uploaded file url, which will be used to read barcodes from
+                Console.WriteLine("Uploaded File Url: {0}", uploadedFileUrl)
+            End If
+
+        Catch ex As WebException
+            Console.WriteLine(ex.ToString())
+        End Try
+
+        webClient.Dispose()
+
+
+        Console.WriteLine()
+        Console.WriteLine("Press any key...")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
