3b1fdc534315c7d5a633508db31628862ec57948
estar73 <estarkov@gmail.com>
2020-02-18 15:08:54 +0000

    = regular update

---
+++ PDF To Image API/C#/Advanced PNG Conversion Options/Program.cs
@@ -0,0 +1,154 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+
+// Cloud API asynchronous "PDF To PNG" job example.
+// Allows to avoid timeout errors when processing huge or scanned PDF documents.
+
+namespace ByteScoutWebApiExample
+{
+	class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co/documentation/api
+		const String API_KEY = "***********************************";
+		
+		// Source PDF file
+		const string SourceFileUrl = @"https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-to-image/sample.pdf";
+		// Comma-separated list of page indices (or ranges) to process. Leave empty for all pages. Example: '0,2-5,7-'.
+		const string Pages = "";
+		// PDF document password. Leave empty for unprotected documents.
+		const string Password = "";
+		// (!) Make asynchronous job
+		const bool Async = true;
+        // Advanced options
+        const string Profiles = "{ 'profiles': [ { 'profile1': { 'ImageBitsPerPixel': 'BPP1' } } ] }";
+
+
+		static void Main(string[] args)
+		{
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			try
+			{
+				// Prepare URL for `PDF To PNG` API call
+				string query = Uri.EscapeUriString(string.Format(
+					"https://api.pdf.co/v1/pdf/convert/to/png?password={0}&pages={1}&url={2}&async={3}&profiles={4}",
+					Password,
+					Pages,
+					SourceFileUrl,
+					Async,
+                    Profiles));
+
+				// Execute request
+				string response = webClient.DownloadString(query);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Asynchronous job ID
+					string jobId = json["jobId"].ToString();
+					// URL of generated JSON file available after the job completion; it will contain URLs of result PNG files.
+					string resultJsonFileUrl = json["url"].ToString();
+
+					// Check the job status in a loop. 
+					// If you don't want to pause the main thread you can rework the code 
+					// to use a separate thread for the status checking and completion.
+					do
+					{
+						string status = CheckJobStatus(jobId); // Possible statuses: "working", "failed", "aborted", "success".
+
+						// Display timestamp and status (for demo purposes)
+						Console.WriteLine(DateTime.Now.ToLongTimeString() + ": " + status);
+
+						if (status == "success")
+						{
+							// Download JSON file as string
+							string jsonFileString = webClient.DownloadString(resultJsonFileUrl);
+
+							JArray resultFilesUrls = JArray.Parse(jsonFileString);
+
+							// Download generated PNG files
+							int page = 1;
+							foreach (JToken token in resultFilesUrls)
+							{
+								string resultFileUrl = token.ToString();
+								string localFileName = String.Format(@".\page{0}.png", page);
+
+								webClient.DownloadFile(resultFileUrl, localFileName);
+
+								Console.WriteLine("Downloaded \"{0}\".", localFileName);
+								page++;
+							}
+							break;
+						}
+						else if (status == "working")
+						{
+							// Pause for a few seconds
+							Thread.Sleep(3000);
+						}
+						else
+						{
+							Console.WriteLine(status);
+							break;
+						}
+					}
+					while (true);
+				}
+				else
+				{
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+
+		static string CheckJobStatus(string jobId)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				// Set API Key
+                webClient.Headers.Add("x-api-key", API_KEY);
+				
+				string url = "https://api.pdf.co/v1/job/check?jobid=" + jobId;
+
+				string response = webClient.DownloadString(url);
+				JObject json = JObject.Parse(response);
+
+				return Convert.ToString(json["status"]);
+			}
+		}
+	}
+}
---
+++ PDF To Image API/C#/Advanced TIFF Conversion Options/Program.cs
@@ -0,0 +1,143 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+
+// Cloud API asynchronous "PDF To TIFF" job example.
+// Allows to avoid timeout errors when processing huge or scanned PDF documents.
+
+namespace ByteScoutWebApiExample
+{
+	class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co/documentation/api
+        const String API_KEY = "***********************************";
+		
+		// Direct URL of source PDF file.
+		const string SourceFileUrl = "https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-to-image/sample.pdf";
+		// Comma-separated list of page indices (or ranges) to process. Leave empty for all pages. Example: '0,2-5,7-'.
+		const string Pages = "";
+		// PDF document password. Leave empty for unprotected documents.
+		const string Password = "";
+		// Destination TIFF file name
+		const string DestinationFile = @".\result.tif";
+		// (!) Make asynchronous job
+		const bool Async = true;
+        // Advanced options
+        const string Profiles = "{ 'profiles': [ { 'profile1': { 'TIFFCompression': 'CCITT4' } } ] }";
+
+		static void Main(string[] args)
+		{
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			// Prepare URL for `PDF To TIFF` API call
+			string query = Uri.EscapeUriString(string.Format(
+				"https://api.pdf.co/v1/pdf/convert/to/tiff?name={0}&password={1}&pages={2}&url={3}&async={4}&profiles={5}", 
+				Path.GetFileName(DestinationFile),
+				Password,
+				Pages,
+				SourceFileUrl,
+				Async,
+                Profiles));
+
+			try
+			{
+				// Execute request
+				string response = webClient.DownloadString(query);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Asynchronous job ID
+					string jobId = json["jobId"].ToString();
+					// URL of generated TIFF file that will available after the job completion
+					string resultFileUrl = json["url"].ToString();
+
+					// Check the job status in a loop. 
+					// If you don't want to pause the main thread you can rework the code 
+					// to use a separate thread for the status checking and completion.
+					do
+					{
+						string status = CheckJobStatus(jobId); // Possible statuses: "working", "failed", "aborted", "success".
+
+						// Display timestamp and status (for demo purposes)
+						Console.WriteLine(DateTime.Now.ToLongTimeString() + ": " + status);
+
+						if (status == "success")
+						{
+							// Download TIFF file
+							webClient.DownloadFile(resultFileUrl, DestinationFile);
+
+							Console.WriteLine("Generated TIFF file saved as \"{0}\" file.", DestinationFile);
+							break;
+						}
+						else if (status == "working")
+						{
+							// Pause for a few seconds
+							Thread.Sleep(3000);
+						}
+						else
+						{
+							Console.WriteLine(status);
+							break;
+						}
+					}
+					while (true);
+				}
+				else
+				{
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+
+		static string CheckJobStatus(string jobId)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				// Set API Key
+                webClient.Headers.Add("x-api-key", API_KEY);
+				
+				string url = "https://api.pdf.co/v1/job/check?jobid=" + jobId;
+
+				string response = webClient.DownloadString(url);
+				JObject json = JObject.Parse(response);
+
+				return Convert.ToString(json["status"]);
+			}
+		}
+	}
+}
