8ce07114023f9666b00550b668a15da1e97a1899
estar73 <estarkov@gmail.com>
2022-08-04 05:42:15 +0000

    = regular update

---
+++ Translate PDF/C#/Translate PDF From URL Asynchronously/Program.cs
@@ -0,0 +1,154 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ByteScoutWebApiExample
+{
+	class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co
+		const String API_KEY = "***********************************";
+		
+		// Direct URL of source PDF file.
+        // You can also upload your own file into PDF.co and use it as url. Check "Upload File" samples for code snippets: https://github.com/bytescout/pdf-co-api-samples/tree/master/File%20Upload/
+		const string SourceFileUrl = "https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-to-excel/sample.pdf";
+
+		// Destination PDF file name
+		const string DestinationFile = @".\result.pdf";
+
+		// Pages to translate. Default translates the 1st page only. Use 0- for all pages
+		const string pagesToTranslate = "0-";
+
+		// From language
+		const string langfrom = "en";
+
+		// To language 
+		const string langTo = "es";
+
+		// (!) Make asynchronous job
+		const bool Async = true;
+
+
+		static void Main(string[] args)
+		{
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			// URL of `PDF Translate` API call
+			string url = "https://api.pdf.co/v1/pdf/translate";
+
+			// Prepare requests params as JSON
+			// See documentation: https://apidocs.pdf.co/02-pdf-translate
+			Dictionary<string, object> parameters = new Dictionary<string, object>();
+			parameters.Add("name", Path.GetFileName(DestinationFile));
+			parameters.Add("url", SourceFileUrl);
+			parameters.Add("pages", pagesToTranslate);
+			parameters.Add("langFrom", langfrom);
+			parameters.Add("langto", langTo);
+			parameters.Add("async", Async);
+
+			// Convert dictionary of params to JSON
+			string jsonPayload = JsonConvert.SerializeObject(parameters);
+
+			try
+			{
+				// Execute POST request with JSON payload
+				string response = webClient.UploadString(url, jsonPayload);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Asynchronous job ID
+					string jobId = json["jobId"].ToString();
+					// URL of generated output file that will available after the job completion
+					string resultFileUrl = json["url"].ToString();
+
+					// Check the job status in a loop. 
+					// If you don't want to pause the main thread you can rework the code 
+					// to use a separate thread for the status checking and completion.
+					do
+					{
+						string status = CheckJobStatus(jobId); // Possible statuses: "working", "failed", "aborted", "success".
+
+						// Display timestamp and status (for demo purposes)
+						Console.WriteLine(DateTime.Now.ToLongTimeString() + ": " + status);
+
+						if (status == "success")
+						{
+							// Download Result file
+							webClient.DownloadFile(resultFileUrl, DestinationFile);
+
+							Console.WriteLine("Translated PDF file saved as \"{0}\" file.", DestinationFile);
+							break;
+						}
+						else if (status == "working")
+						{
+							// Pause for a few seconds
+							Thread.Sleep(3000);
+						}
+						else
+						{
+							Console.WriteLine(status);
+							break;
+						}
+					}
+					while (true);
+				}
+				else
+				{
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+
+		static string CheckJobStatus(string jobId)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				// Set API Key
+                webClient.Headers.Add("x-api-key", API_KEY);
+				
+				string url = "https://api.pdf.co/v1/job/check?jobid=" + jobId;
+
+				string response = webClient.DownloadString(url);
+				JObject json = JObject.Parse(response);
+
+				return Convert.ToString(json["status"]);
+			}
+		}
+	}
+}
---
+++ Translate PDF/C#/Translate PDF From URL/Program.cs
@@ -0,0 +1,104 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ByteScoutWebApiExample
+{
+	class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co
+		const String API_KEY = "***********************************";
+		
+		// Direct URL of source PDF file.
+        // You can also upload your own file into PDF.co and use it as url. Check "Upload File" samples for code snippets: https://github.com/bytescout/pdf-co-api-samples/tree/master/File%20Upload/
+		const string SourceFileUrl = "https://bytescout-com.s3.amazonaws.com/files/demo-files/cloud-api/pdf-to-excel/sample.pdf";
+
+		// Destination PDF file name
+		const string DestinationFile = @".\result.pdf";
+
+		// Pages to translate. Default translates the 1st page only. Use 0- for all pages
+		const string pagesToTranslate = "0-";
+
+		// From language
+		const string langfrom = "en";
+
+		// To language 
+		const string langTo = "es";
+
+		static void Main(string[] args)
+		{
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			// URL of `PDF Translate` API call
+			string url = "https://api.pdf.co/v1/pdf/translate";
+
+			// Prepare requests params as JSON
+			// See documentation: https://apidocs.pdf.co/02-pdf-translate
+			Dictionary<string, object> parameters = new Dictionary<string, object>();
+			parameters.Add("name", Path.GetFileName(DestinationFile));
+			parameters.Add("url", SourceFileUrl);
+			parameters.Add("pages", pagesToTranslate);
+			parameters.Add("langFrom", langfrom);
+			parameters.Add("langto", langTo);
+
+
+			// Convert dictionary of params to JSON
+			string jsonPayload = JsonConvert.SerializeObject(parameters);
+
+			try
+			{
+				// Execute POST request with JSON payload
+				string response = webClient.UploadString(url, jsonPayload);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Get URL of generated output file
+					string resultFileUrl = json["url"].ToString();
+
+					// Download output file
+					webClient.DownloadFile(resultFileUrl, DestinationFile);
+
+					Console.WriteLine("Translated PDF file saved as \"{0}\" file.", DestinationFile);
+				}
+				else
+				{
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+	}
+}
