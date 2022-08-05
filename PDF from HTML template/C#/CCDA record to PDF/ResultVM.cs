dd3be9ec4d17edfaff5bbc17d2ec9d18ca8b42b4
estar73 <estarkov@gmail.com>
2022-08-05 08:52:46 +0000

    = regular update

---
+++ PDF from HTML template/C#/CCDA record to PDF/ResultVM.cs
@@ -0,0 +1,24 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+namespace ByteScoutWebApiExample
+{
+    public class ResultVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ValuePerUnit { get; set; }
+        public string Interpretation { get; set; }
+        public string ReferenceRange { get; set; }
+    }
+}
