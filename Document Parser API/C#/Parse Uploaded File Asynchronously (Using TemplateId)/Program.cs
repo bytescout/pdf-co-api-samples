f27afb9e870f81e4f8d64ca68b803f2c41703f0f
estar73 <estarkov@gmail.com>
2022-08-09 05:05:32 +0000

    = regular update

---
+++ Document Parser API/C#/Parse Uploaded File Asynchronously (Using TemplateId)/Program.cs
@@ -0,0 +1,181 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright Â© 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+// Cloud API asynchronous "Document Parser" job example.
+// Allows to avoid timeout errors when processing huge or scanned PDF documents.
+
+namespace ByteScoutWebApiExample
+{
+    class Program
+	{
+		// The authentication key (API Key).
+		// Get your own by registering at https://app.pdf.co
+		const String API_KEY = "***********************************";
+		
+		// Source PDF file
+		const string SourceFile = @".\sample-invoice.pdf";
+
+		// Destination TXT file name
+		const string DestinationFile = @".\result.json";
+
+        // (!) Make asynchronous job
+        const bool Async = true;
+
+
+		static void Main(string[] args)
+		{
+            // --TemplateID--
+            /* 
+				Please follow below steps to create your own Template and get "templateId". 
+				1. Use Document Parser to create Template Id https://app.pdf.co/document-parser
+				2. Go to https://app.pdf.co/document-parser and create new template.
+				3. Save this new template
+				4. Copy it’s ID to clipboard
+				5. Now set ID of the template into “templateId” parameter
+			*/
+            String templateId = "1";
+
+			// Create standard .NET web client instance
+			WebClient webClient = new WebClient();
+
+			// Set API Key
+			webClient.Headers.Add("x-api-key", API_KEY);
+
+			// 1. RETRIEVE THE PRESIGNED URL TO UPLOAD THE FILE.
+			// * If you already have a direct file URL, skip to the step 3.
+			
+			// Prepare URL for `Get Presigned URL` API call
+			string query = Uri.EscapeUriString(string.Format(
+				"https://api.pdf.co/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name={0}", 
+				Path.GetFileName(SourceFile)));
+
+			try
+			{
+				// Execute request
+				string response = webClient.DownloadString(query);
+
+				// Parse JSON response
+				JObject json = JObject.Parse(response);
+
+				if (json["error"].ToObject<bool>() == false)
+				{
+					// Get URL to use for the file upload
+					string uploadUrl = json["presignedUrl"].ToString();
+					string uploadedFileUrl = json["url"].ToString();
+
+					// 2. UPLOAD THE FILE TO CLOUD.
+
+					webClient.Headers.Add("content-type", "application/octet-stream");
+					webClient.UploadFile(uploadUrl, "PUT", SourceFile); // You can use UploadData() instead if your file is byte[] or Stream
+					webClient.Headers.Remove("content-type");
+
+                    // 3. PARSE UPLOADED PDF DOCUMENT
+
+                    // URL of `Document Parser` API call
+                    string url = "https://api.pdf.co/v1/pdf/documentparser";
+
+                    Dictionary<string, object> requestBody = new Dictionary<string, object>();
+                    requestBody.Add("templateId", templateId);
+                    requestBody.Add("name", Path.GetFileName(DestinationFile));
+                    requestBody.Add("url", uploadedFileUrl);
+                    requestBody.Add("async", Async);
+
+                    // Convert dictionary of params to JSON
+                    string jsonPayload = JsonConvert.SerializeObject(requestBody);
+
+                    // Execute request
+                    response = webClient.UploadString(url, "POST", jsonPayload);
+                    
+                    // Parse JSON response
+                    json = JObject.Parse(response);
+
+                    if (json["error"].ToObject<bool>() == false)
+                    {
+                        // Asynchronous job ID
+                        string jobId = json["jobId"].ToString();
+                        // Get URL of generated JSON file
+                        string resultFileUrl = json["url"].ToString();
+
+                        // Check the job status in a loop. 
+                        // If you don't want to pause the main thread you can rework the code 
+                        // to use a separate thread for the status checking and completion.
+                        do
+                        {
+                            string status = CheckJobStatus(webClient, jobId); // Possible statuses: "working", "failed", "aborted", "success".
+
+                            // Display timestamp and status (for demo purposes)
+                            Console.WriteLine(DateTime.Now.ToLongTimeString() + ": " + status);
+
+                            if (status == "success")
+                            {
+                                // Download JSON file
+                                webClient.DownloadFile(resultFileUrl, DestinationFile);
+
+                                Console.WriteLine("Generated JSON file saved as \"{0}\" file.", DestinationFile);
+                                break;
+                            }
+                            else if (status == "working")
+                            {
+                                // Pause for a few seconds
+                                Thread.Sleep(3000);
+                            }
+                            else 
+                            {
+                                Console.WriteLine(status);
+                                break;
+                            }
+                        }
+                        while (true);
+                    }
+                    else
+                    {
+                        Console.WriteLine(json["message"].ToString());
+                    }
+				}
+				else
+				{
+					Console.WriteLine(json["message"].ToString());
+				}
+			}
+			catch (WebException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			webClient.Dispose();
+
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+
+        static string CheckJobStatus(WebClient webClient, string jobId)
+        {
+            string url = "https://api.pdf.co/v1/job/check?jobid=" + jobId;
+
+            string response = webClient.DownloadString(url);
+            JObject json = JObject.Parse(response);
+
+            return Convert.ToString(json["status"]);
+        }
+	}
+}
