d4ebec353dc5443813053f3009bfe66614cbbb9b
estar73 <estarkov@gmail.com>
2022-11-14 07:56:29 +0000

    = regular update

--- PDF To XLSX/PHP/Convert PDF To XLSX From Uploaded File/pdf-to-excel.php
+++ PDF To XLSX/PHP/Convert PDF To XLSX From Uploaded File/pdf-to-excel.php
@@ -44,7 +44,8 @@ if (curl_errno($curl) == 0)
         $uploadedFileUrl = $json["url"];
         
         // 2. UPLOAD THE FILE TO CLOUD.
-        
+        //echo json_encode($_FILES["file"]);
+        //print_r($_FILES["file"]);
         $localFile = $_FILES["file"]["tmp_name"];
         $fileHandle = fopen($localFile, "r");
         
@@ -107,6 +108,7 @@ function ExtractExcel($apiKey, $uploadedFileUrl, $pages)
     $parameters = array();
     $parameters["url"] = $uploadedFileUrl;
     $parameters["pages"] = $pages;
+$parameters["async"] = true;  // (!) Make asynchronous job
 
     // Create Json payload
     $data = json_encode($parameters);
@@ -132,10 +134,37 @@ function ExtractExcel($apiKey, $uploadedFileUrl, $pages)
             
             if (!isset($json["error"]) || $json["error"] == false)
             {
-                $resultFileUrl = $json["url"];
+	            // URL of generated XLSX file that will available after the job completion
+	            $resultFileUrl = $json["url"];
+	            // Asynchronous job ID
+	            $jobId = $json["jobId"];
+            
+	            // Check the job status in a loop
+	            do
+	            {
+	                $status = CheckJobStatus($jobId, $apiKey); // Possible statuses: "working", "failed", "aborted", "success".
                 
-                // Display link to the file with conversion results
-                echo "<div><h2>Conversion Result:</h2><a href='" . $resultFileUrl . "' target='_blank'>" . $resultFileUrl . "</a></div>";
+	                // Display timestamp and status (for demo purposes)
+	                echo "<p>" . date(DATE_RFC2822) . ": " . $status . "</p>";
+                
+	                if ($status == "success")
+	                {
+	                    // Display link to the file with conversion results
+	                    echo "<div><h2>Conversion Result:</h2><a href='" . $resultFileUrl . "' target='_blank'>" . $resultFileUrl . "</a></div>";
+	                    break;
+	                }
+	                else if ($status == "working")
+	                {
+	                    // Pause for a few seconds
+	                    sleep(3);
+	                }
+	                else 
+	                {
+	                    echo $status . "<br/>";
+	                    break;
+	                }
+	            }
+	            while (true);
             }
             else
             {
@@ -158,6 +187,68 @@ function ExtractExcel($apiKey, $uploadedFileUrl, $pages)
     
     // Cleanup
     curl_close($curl);
+}
+
+function CheckJobStatus($jobId, $apiKey)
+{
+    $status = null;
+    
+	// Create URL
+    $url = "https://api.pdf.co/v1/job/check";
+    
+    // Prepare requests params
+    $parameters = array();
+    $parameters["jobid"] = $jobId;
+
+    // Create Json payload
+    $data = json_encode($parameters);
+
+    // Create request
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey, "Content-type: application/json"));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    
+    // Execute request
+    $result = curl_exec($curl);
+    
+    if (curl_errno($curl) == 0)
+    {
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        if ($status_code == 200)
+        {
+            $json = json_decode($result, true);
+        
+            if (!isset($json["error"]) || $json["error"] == false)
+            {
+                $status = $json["status"];
+            }
+            else
+            {
+                // Display service reported error
+                echo "<p>Error: " . $json["message"] . "</p>"; 
+            }
+        }
+        else
+        {
+            // Display request error
+            echo "<p>Status code: " . $status_code . "</p>"; 
+            echo "<p>" . $result . "</p>"; 
+        }
+    }
+    else
+    {
+        // Display CURL error
+        echo "Error: " . curl_error($curl);
+    }
+    
+    // Cleanup
+    curl_close($curl);
+    
+    return $status;
 }
 
 ?>
