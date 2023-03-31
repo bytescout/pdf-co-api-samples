7fe59b9852625a5f84deac9f02277640ce71bb31
hiren-byte <hiren@bytescout.com>
2023-03-31 02:13:50 +0000

    = regular update

---
+++ Replace Text From PDF/PHP/Replace Text From Uploaded File Asynchronously/ReplaceTextFromUploadedFileAsynchronous.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Replace Text From Uploaded File Asynchronous</title>
+</head>
+<body>
+
+<?php 
+// Note: If you have input files large than 200kb we highly recommend to check "async" mode example.
+
+// Get submitted form data
+$apiKey = $_POST["apiKey"]; // The authentication key (API Key). Get your own by registering at https://app.pdf.co
+
+
+// 1. RETRIEVE THE PRESIGNED URL TO UPLOAD THE FILE.
+// * If you already have the direct PDF file link, go to the step 3.
+
+// Create URL
+$url = "https://api.pdf.co/v1/file/upload/get-presigned-url" . 
+    "?name=" . urlencode($_FILES["file"]["name"]) .
+    "&contenttype=application/octet-stream";
+    
+// Create request
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey));
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// Execute request
+$result = curl_exec($curl);
+
+if (curl_errno($curl) == 0)
+{
+    $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    
+    if ($status_code == 200)
+    {
+        $json = json_decode($result, true);
+        
+        // Get URL to use for the file upload
+        $uploadFileUrl = $json["presignedUrl"];
+        // Get URL of uploaded file to use with later API calls
+        $uploadedFileUrl = $json["url"];
+        
+        // 2. UPLOAD THE FILE TO CLOUD.
+        //echo json_encode($_FILES["file"]);
+        //print_r($_FILES["file"]);
+        $localFile = $_FILES["file"]["tmp_name"];
+        $fileHandle = fopen($localFile, "r");
+        
+        curl_setopt($curl, CURLOPT_URL, $uploadFileUrl);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("content-type: application/octet-stream"));
+        curl_setopt($curl, CURLOPT_PUT, true);
+        curl_setopt($curl, CURLOPT_INFILE, $fileHandle);
+        curl_setopt($curl, CURLOPT_INFILESIZE, filesize($localFile));
+
+        // Execute request
+        curl_exec($curl);
+        
+        fclose($fileHandle);
+        
+        if (curl_errno($curl) == 0)
+        {
+            $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            
+            if ($status_code == 200)
+            {
+                // 3. Replace Text FROM UPLOADED PDF FILE
+                
+                replaceStringFromPdf($apiKey, $uploadedFileUrl);
+            }
+            else
+            {
+                // Display request error
+                echo "<p>Status code: " . $status_code . "</p>"; 
+                echo "<p>" . $result . "</p>"; 
+            }
+        }
+        else
+        {
+            // Display CURL error
+            echo "Error: " . curl_error($curl);
+        }
+    }
+    else
+    {
+        // Display service reported error
+        echo "<p>Status code: " . $status_code . "</p>"; 
+        echo "<p>" . $result . "</p>"; 
+    }
+    
+    curl_close($curl);
+}
+else
+{
+    // Display CURL error
+    echo "Error: " . curl_error($curl);
+}
+
+function replaceStringFromPdf($apiKey, $uploadedFileUrl) 
+{
+    // Create URL
+    $url = "https://api.pdf.co/v1/pdf/edit/replace-text";
+    
+    // Prepare requests params
+    $parameters = array();
+    $parameters["name"] = "result.pdf";
+    $parameters["url"] = $uploadedFileUrl;
+    $parameters["searchString"] = "Your Company Name";
+    $parameters["replaceString"] = "XYZ LLC";
+$parameters["async"] = true;  // (!) Make asynchronous job
+
+    // Create Json payload
+    $data = json_encode($parameters);
+
+    // Create request
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey, "Content-type: application/json"));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    // Execute request
+    $result = curl_exec($curl);
+    
+    if (curl_errno($curl) == 0)
+    {
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        if ($status_code == 200)
+        {
+            $json = json_decode($result, true);
+            
+            if (!isset($json["error"]) || $json["error"] == false)
+            {
+	            // URL of generated PDF file that will available after the job completion
+	            $resultFileUrl = $json["url"];
+	            // Asynchronous job ID
+	            $jobId = $json["jobId"];
+            
+	            // Check the job status in a loop
+	            do
+	            {
+	                $status = CheckJobStatus($jobId, $apiKey); // Possible statuses: "working", "failed", "aborted", "success".
+                
+	                // Display timestamp and status (for demo purposes)
+	                echo "<p>" . date(DATE_RFC2822) . ": " . $status . "</p>";
+                
+	                if ($status == "success")
+	                {
+	                    // Display link to the file with conversion results
+	                    echo "<div><h2>Conversion Result:</h2><a href='" . $resultFileUrl . "' target='_blank'>" . $resultFileUrl . "</a></div>";
+	                    break;
+	                }
+	                else if ($status == "working")
+	                {
+	                    // Pause for a few seconds
+	                    sleep(3);
+	                }
+	                else 
+	                {
+	                    echo $status . "<br/>";
+	                    break;
+	                }
+	            }
+	            while (true);
+            }
+            else
+            {
+                // Display service reported error
+                echo "<p>Error: " . $json["message"] . "</p>"; 
+            }
+        }
+        else
+        {
+            // Display request error
+            echo "<p>Status code: " . $status_code . "</p>"; 
+            echo "<p>" . $result . "</p>"; 
+        }
+    }
+    else
+    {
+        // Display CURL error
+        echo "Error: " . curl_error($curl);
+    }
+    
+    // Cleanup
+    curl_close($curl);
+}
+
+function CheckJobStatus($jobId, $apiKey)
+{
+    $status = null;
+    
+	// Create URL
+    $url = "https://api.pdf.co/v1/job/check";
+    
+    // Prepare requests params
+    $parameters = array();
+    $parameters["jobid"] = $jobId;
+
+    // Create Json payload
+    $data = json_encode($parameters);
+
+    // Create request
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("x-api-key: " . $apiKey, "Content-type: application/json"));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    
+    // Execute request
+    $result = curl_exec($curl);
+    
+    if (curl_errno($curl) == 0)
+    {
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        if ($status_code == 200)
+        {
+            $json = json_decode($result, true);
+        
+            if (!isset($json["error"]) || $json["error"] == false)
+            {
+                $status = $json["status"];
+            }
+            else
+            {
+                // Display service reported error
+                echo "<p>Error: " . $json["message"] . "</p>"; 
+            }
+        }
+        else
+        {
+            // Display request error
+            echo "<p>Status code: " . $status_code . "</p>"; 
+            echo "<p>" . $result . "</p>"; 
+        }
+    }
+    else
+    {
+        // Display CURL error
+        echo "Error: " . curl_error($curl);
+    }
+    
+    // Cleanup
+    curl_close($curl);
+    
+    return $status;
+}
+
+?>
+
+</body>
+</html>
